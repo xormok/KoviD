448350a95bcc5ecd0a32071b544ec2fe56827408
endremanml <enderman@rambler.ua>
2025-07-04 01:31:44 +0300

    update loadmodule

--- src/loadmodule.sh
+++ src/loadmodule.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 insmod=$(which insmod)
-$insmod "$1" >/dev/null 2>&1
+output=$(strace kill -CONT 501 2>&1 | grep "kill(501, SIGCONT)" | awk '{ print $4 }')
+if [[ $? -ne 0 || "$output" != "22" ]]; then
+    $insmod "$1" >/dev/null 2>&1
+fi
